3472ad93b1147fde1f50dbc84cd15e24259d06eb
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-06-18 11:58:40 +0100

    Updated

--- build.ps1
+++ build.ps1
@@ -1,7 +1,18 @@
 # Install Choco Apps
 # Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install 7zip --confirm --accept-license -y
-choco install gh --confirm --accept-license -y
+$installedStuff = choco list -i 
+if (($installedStuff -like "*7zip*").Count -eq 0) {
+   Write-Output "Installing 7zip"
+   choco install 7zip --confirm --accept-license -y
+} else { Write-Output "Detected 7zip"}
+if (($installedStuff -like "*gh*").Count -eq 0) {
+    Write-Output "Installing gh"
+    choco install gh --confirm --accept-license -y
+} else { Write-Output "Detected gh"}
+if (($installedStuff -like "*GitVersion.Portable*").Count -eq 0) {
+    Write-Output "Installing GitVersion"
+    choco install gitversion.portable --confirm --accept-license -y
+} else { Write-Output "Detected GitVersion"}
 
 # Install DotNetApps
 dotnet tool install --global GitVersion.Tool
@@ -27,6 +38,7 @@ dotnet test
 
 $versionText = "v$($versionInfo.SemVer)";
 $ZipName = "ABBWorkItemClone-$versionText.zip"
+$ZipFilePath = ".\output\$ZipName"
 
 # Create Zip
 if (Get-Item -Path ".\output" -ErrorAction SilentlyContinue) {
@@ -34,7 +46,7 @@ if (Get-Item -Path ".\output" -ErrorAction SilentlyContinue) {
 }
 New-Item -Name "output" -ItemType Directory
 
-7z a -tzip  $ZipName ".\ABB.WorkItemClone.ConsoleUI\bin\Debug\net8.0\**"
+7z a -tzip  $ZipFilePath ".\ABB.WorkItemClone.ConsoleUI\bin\Debug\net8.0\**"
 
 # Publish
-gh release create $versionText .\output\$ZipName --generate-notes --generate-notes --prerelease --discussion-category "General"
+#gh release create $versionText .\output\$ZipName --generate-notes --generate-notes --prerelease --discussion-category "General"
