a3cb99b735265d9caa21933ead3c340eefcbc3da
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-07-03 16:47:13 +0100

    Add starter Open Telemitery

--- AzureDevOps.WorkItemClone.ConsoleUI/Program.cs
+++ AzureDevOps.WorkItemClone.ConsoleUI/Program.cs
@@ -9,13 +9,43 @@ using Spectre.Console.Cli;
 using AzureDevOps.WorkItemClone.ConsoleUI.Commands;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTelemetry;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Metrics;
 
 namespace ABB.WorkItemClone.ConsoleUI
 {
     internal class Program
     {
+        private const string ConnectionString = "InstrumentationKey=6b3339ba-05e1-447e-ab55-32c34cf9998e;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=0e7b1578-b095-423f-9644-63dbd081d493";
+
         static async Task<int> Main(string[] args)
         {
+            // Create a new tracer provider builder and add an Azure Monitor trace exporter to the tracer provider builder.
+            // It is important to keep the TracerProvider instance active throughout the process lifetime.
+            // See https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/trace#tracerprovider-management
+            var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAzureMonitorTraceExporter(o => o.ConnectionString = ConnectionString);
+
+            // Add an Azure Monitor metric exporter to the metrics provider builder.
+            // It is important to keep the MetricsProvider instance active throughout the process lifetime.
+            // See https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/metrics#meterprovider-management
+            var metricsProvider = Sdk.CreateMeterProviderBuilder()
+                .AddRuntimeInstrumentation()
+                .AddAzureMonitorMetricExporter(o => o.ConnectionString = ConnectionString);
+            // Create a new logger factory.
+            // It is important to keep the LoggerFactory instance active throughout the process lifetime.
+            // See https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/logs#logger-management
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddOpenTelemetry(options =>
+                {
+                    options.AddAzureMonitorLogExporter(o => o.ConnectionString = ConnectionString);
+                });
+            });
+
+
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
 
